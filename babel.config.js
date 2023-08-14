9d1829a32bc05a286d6951de9662727afa730173
miyasan31 <miyasan.dev@gmail.com>
2023-08-15 04:38:15 +0900

    chore(config): babel path alias

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,24 @@
+const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            $navigation: path.resolve(__dirname, './src/navigation'),
+            $components: path.resolve(__dirname, './src/components'),
+            $constants: path.resolve(__dirname, './src/shared/constants'),
+            $hooks: path.resolve(__dirname, './src/shared/hooks'),
+            $libs: path.resolve(__dirname, './src/shared/libs'),
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
