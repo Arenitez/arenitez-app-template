4b20ae23414859d3722d996c241289e64f64ffe0
miyasan31 <miyasan.dev@gmail.com>
2023-08-27 00:10:13 +0900

    chore(config): remove path alias

--- babel.config.js
+++ babel.config.js
@@ -4,21 +4,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.tsx', '.ts', '.js', '.json'],
-          alias: {
-            $navigation: path.resolve(__dirname, './src/navigation'),
-            $components: path.resolve(__dirname, './src/components'),
-            $constants: path.resolve(__dirname, './src/shared/constants'),
-            $hooks: path.resolve(__dirname, './src/shared/hooks'),
-            $libs: path.resolve(__dirname, './src/shared/libs'),
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
