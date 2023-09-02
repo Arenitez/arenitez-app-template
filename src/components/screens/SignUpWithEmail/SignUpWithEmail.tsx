dd6600e1ee4675a8ddd3997096a5e80f7734dcdf
miyasan31 <miyasan.dev@gmail.com>
2023-09-02 14:49:23 +0900

    feat: sign in,up email

--- src/components/screens/SignUpWithEmail/SignUpWithEmail.tsx
+++ src/components/screens/SignUpWithEmail/SignUpWithEmail.tsx
@@ -1,25 +1,46 @@
 import { useNavigation } from '@react-navigation/core';
 import { NavigationProp } from '@react-navigation/core/src/types';
-import { createAreniteStyle, SafeAreaView } from 'arenite-kit';
+import { createAreniteStyle, SafeAreaView, useToast } from 'arenite-kit';
 
 import { useAuth } from '$components/providers/AuthProvider';
 import { SignForm, SignFormValue } from '$components/shared/form/SignForm';
+import { ToastId } from '$libs/arenite-kit/toastId';
+import { firebaseAuth } from '$libs/firebase/auth';
 import { getSafeAreaEdges } from '$libs/react-native-safe-area-context/getSafeAreaEdges';
 import { RootParamList } from '$navigation/navigate';
 
+const { signUpWithEmail, catchFirebaseAuthError } = firebaseAuth;
+
 export const SignUpWithEmailScreen = () => {
   const { signIn } = useAuth();
+  const toast = useToast();
   const navigation = useNavigation<NavigationProp<RootParamList>>();
   const edges = getSafeAreaEdges('horizontal');
 
-  const onPressSignUpButton = (_formValues: SignFormValue) => {
-    signIn();
-    navigation.navigate('AppNavigator');
+  const onPressSignUpButton = async (values: SignFormValue) => {
+    try {
+      const result = await signUpWithEmail(values);
+      signIn(result.user);
+
+      navigation.navigate('AppNavigator');
+      toast.addToast({
+        id: ToastId.SignUpEmailSuccess,
+        type: 'success',
+        message: 'サインアップに成功しました',
+      });
+    } catch (error) {
+      const errorMessage = catchFirebaseAuthError(error);
+      toast.addToast({
+        id: ToastId.SignUpEmailError,
+        type: 'error',
+        message: errorMessage,
+      });
+    }
   };
 
   return (
     <SafeAreaView style={style.container} edges={edges} bg={'bg1'}>
-      <SignForm formType={'sign-in'} onSubmit={onPressSignUpButton} />
+      <SignForm formType={'sign-up'} onSubmit={onPressSignUpButton} />
     </SafeAreaView>
   );
 };
---
+++ src/shared/libs/arenite-kit/toastId.ts
@@ -0,0 +1,6 @@
+export const ToastId = {
+  SignInEmailSuccess: 'SIGN_IN_EMAIL_SUCCESS',
+  SignInEmailError: 'SIGN_IN_EMAIL_ERROR',
+  SignUpEmailSuccess: 'SIGN_UP_EMAIL_SUCCESS',
+  SignUpEmailError: 'SIGN_UP_EMAIL_ERROR',
+} as const;
